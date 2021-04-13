daf0f3a6a16983e658461c13a31594c9456112d2
Karlo Siladi <siladikarlo856@gmail.com>
2021-04-13 22:31:19 +0200

    First 4 exercises

---
+++ ex3_sd1.py
@@ -0,0 +1,38 @@
+# Print string
+print "I will now count my chickens:"
+
+# Calculate 25 + 30 / 6
+print "Hens", 25 + 30 / 6
+# Calculate 100 - 25 * 3 % 4
+print "Roosters", 100 - 25 * 3 % 4
+
+# Print string
+print "Now I will count the eggs:"
+
+# Calculate expression
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# Print string
+print "Is it true that 3 + 2 < 5 - 7"
+
+# Check i (3 + 2) is less than (5 - 7)
+print 3 + 2 < 5 - 7
+
+# Calculate 3 + 2
+print "What is 3 + 2?", 3 + 2
+# calculate 5 - 7
+print "What is 5 - 7?", 5 - 7
+
+# Print string
+print "Oh, that's why it's False."
+
+# Print string
+print "How about some more."
+
+# Check if 5 is greater than -2
+print "Is it grater?", 5 > -2
+# Check if 5 is greater or equal to -2
+print "Is it greater or equal", 5 >= -2
+# Check if 5 is less or equal to -2
+print "Is it less or equal", 5 <= -2
+
---
+++ ex3_sd3.py
@@ -0,0 +1,6 @@
+print "What is 2 + 2?"
+print "2 + 2 = ", 2 + 2
+
+print "7.0/4.0 vs 7/4"
+print 7.0/4.0, " vs ", 7/4
+
